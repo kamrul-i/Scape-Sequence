0f38c8bc5213d0b697c4860a1c77cff72296fdcc
kamrul-i <komrul45@gmail.com>
2024-04-05 16:13:18 +0600

    First commit

---
+++ Scape_Sequence/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Scape_Sequence {
+}
---
+++ Scape_Sequence/src/scape_sequence/Scape_Sequence.java
@@ -0,0 +1,32 @@
+package scape_sequence;
+
+public class Scape_Sequence {
+	public static void main(String[] args) {
+		
+		
+		
+		//"Scape-Sequence" is must be inside the (double quote / inverted comma).
+		
+		//use of "\t" -(tab)  (it means tab-space).
+		System.out.println(" 1 \t 2 \t 3 \t 4 ");		
+		//use of "\b" -(backspace)
+		
+		//use of "\n" -(new line)
+		System.out.println(" Bangladesh is an independence country. \n I am proud of my country.");
+		
+		//use of "\r" -(carriage return)
+		System.out.println(" \rBangladesh\r is an independence country. I am proud of my country.");
+		
+		//use of (\") -(double quote)
+		System.out.println(" \"Bangladesh\" is an independence country. I am proud of my country.");
+		
+		//use of (\') -(single quote)
+		System.out.println(" \'Bangladesh\' is an independence country. I am proud of my country.");
+		
+		//use of (\\) - (backslash)
+		System.out.println(" \\Bangladesh\\ is an independence country. I am proud of my country.");
+		
+		
+	}
+
+}
